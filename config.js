3af8c75cd7d7772216f0603cc4a02ae8c9d11682
qwerty9307111 <2436415068@qq.com>
2019-05-13 23:56:06 +0800

    feat: config

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  cookie: '_yapi_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjMxLCJpYXQiOjE1NTc3NDc2MDgsImV4cCI6MTU1ODM1MjQwOH0.RyQd1Oc09-XAlr74HEbTko4mWtdueD9O6hd8mXPbVjo; _yapi_uid=31',
+  dirname: './dist', // 输出目录
+  fileName: 'api.ts', // api 文件名
+  requestUrl: 'http://172.27.0.5:3000/api/interface/list_menu?project_id=95'
+}
--- src/index.js
+++ src/index.js
@@ -1,39 +1,40 @@
 const axios = require('axios')
 const fs = require('fs')
-
-const _dirname = './dist' // 输出目录
-const Cookie = '_yapi_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjMxLCJpYXQiOjE1NTc3NDc2MDgsImV4cCI6MTU1ODM1MjQwOH0.RyQd1Oc09-XAlr74HEbTko4mWtdueD9O6hd8mXPbVjo; _yapi_uid=31'
+const config = require('../config')
 
 let exportApiArr = []
 
 listMenu()
 
 function listMenu () {
-  axios.get('http://172.27.0.5:3000/api/interface/list_menu?project_id=95', {
-    headers: { Cookie }
+  axios.get(config.requestUrl, {
+    headers: {
+      Cookie: config.cookie
+    }
   }).then(res => {
     if (res.data.errcode === 0) {
-      async function request () {
-        await appendImportCode()
-        let code = ''
-        res.data.data.map(item => {
-          let desc = item.name
-          console.log(desc)
-          item.list.map(api => {
-            let apiName = changeCase(api.path) + '_' + api.method.toLowerCase()
-            exportApiArr.push(apiName)
-            code += `\n/** ${desc} ${api.title.trim()} */\nfunction ${apiName} (opts?: AxiosRequestConfig) {\n  return instance({\n    method: '${api.method.toLowerCase()}',\n    url: '/api/v4/manager${api.path}',\n    ...opts\n  })\n}\n`
-          })
-        })
-        await fs.appendFile(_dirname + '/api.ts', code, () => {
-          console.log('WRITE FILE SUCCESS')
-          appendExportCode()
-        })
-      }
-      request()
+      appendApiCode(res.data.data)
     } else {
       console.log(res.data)
     }
+  })
+}
+
+async function appendApiCode (data) {
+  await appendImportCode()
+  let code = ''
+  data.map(item => {
+    let desc = item.name
+    console.log(desc)
+    item.list.map(api => {
+      let apiName = changeCase(api.path) + '_' + api.method.toLowerCase()
+      exportApiArr.push(apiName)
+      code += `\n/** ${desc} ${api.title.trim()} */\nfunction ${apiName} (opts?: AxiosRequestConfig) {\n  return instance({\n    method: '${api.method.toLowerCase()}',\n    url: '/api/v4/manager${api.path}',\n    ...opts\n  })\n}\n`
+    })
+  })
+  await fs.appendFile(config.dirname + '/' + config.fileName, code, () => {
+    console.log('WRITE FILE SUCCESS')
+    appendExportCode()
   })
 }
 
@@ -48,10 +49,15 @@ function changeCase (str) {
 function appendImportCode () {
   const headerCode = `import { AxiosRequestConfig } from 'axios'\nimport instance from './instance'\n`
 
-  fs.truncateSync(_dirname + '/api.ts', 0)
-  console.log('清除文件成功')
-
-  fs.writeFileSync(_dirname + '/api.ts', headerCode)
+  const exists = fs.existsSync(config.dirname + '/' + config.fileName)
+  if (exists) {
+    fs.truncateSync(config.dirname + '/' + config.fileName, 0)
+    console.log('清除文件成功')
+  } else {
+    fs.mkdirSync(config.dirname)
+    console.log('创建目录成功')
+  }
+  fs.writeFileSync(config.dirname + '/' + config.fileName, headerCode)
   console.log('写入文件成功')
 }
 
@@ -65,7 +71,7 @@ function appendExportCode () {
       api += name
     }
   })
-  fs.appendFile(_dirname + '/api.ts', `\nexport {\n  ${api}\n}\n`, () => {
+  fs.appendFile(config.dirname + '/' + config.fileName, `\nexport {\n  ${api}\n}\n`, () => {
     console.log('THE END!')
   })
 }
